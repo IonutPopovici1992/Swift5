cc83252a9ad7996f605c7c78e8ad077e49443478
John <ionut_popovici92@yahoo.com>
2020-01-28 16:32:12 +0200

    App: Smack - Project Setup

---
+++ Smack/Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by John on 1/27/20.
+//  Copyright © 2020 John. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
